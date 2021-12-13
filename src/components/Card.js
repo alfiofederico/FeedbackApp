c2c076fc26a305800333f301b1d44d2a4e6f4447
Alfio Federico Di Pinto <fefferico81@gmail.com>
2021-12-13 20:57:34 +0900

    working on the mode selection bug

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,20 @@
+
+import { FaLightbulb } from "react-icons/fa";
+
+
 function Card({children, dark}) {
+ const addclass = () => {
+   document.querySelector("#changeclass").classList.toggle("dark");
+ }; 
+
   return (
-      <div className={`card ${dark && "dark"}`}>
-         
-        {children}
-        </div>
-  
+    <div className={`card ${dark && "dark"}`} id="changeclass">
+      <FaLightbulb size={30} onClick={addclass} />
+
+      {children}
+    </div>
   );
-}
+} 
 
 Card.defaultProps = {
   dark: false,
