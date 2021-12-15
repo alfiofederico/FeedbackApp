24dd58bb5504a012f62c07eb79cf0c1bd5afbb2a
Alfio Federico Di Pinto <fefferico81@gmail.com>
2021-12-15 10:30:19 +0900

    added the spinner, fixed the edits

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -7,77 +7,76 @@ import FeedbackContext from "../context/FeedbackContext"
 
 
 function FeedbackForm() {
-
- 
-  const [text, setText] = useState('')
-  const [btnDisabled, setBtnDisabled] = useState(true)
-  const [message, setMessage] =useState('')
+  const [text, setText] = useState("");
   const [rating, setRating] = useState(10);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
-  const { addFeedback, feedbackEdit, updateFeedback } = useContext(FeedbackContext);
+  const { addFeedback, feedbackEdit, updateFeedback } =
+    useContext(FeedbackContext);
 
   useEffect(() => {
-    if(feedbackEdit.edit === true) {
-      setBtnDisabled(false)
-      setText(feedbackEdit.item.text)
-      setRating(feedbackEdit.item.rating)
+    if (feedbackEdit.edit === true) {
+      setBtnDisabled(false);
+      setText(feedbackEdit.item.text);
+      setRating(feedbackEdit.item.rating);
     }
-  }, [feedbackEdit])
+  }, [feedbackEdit]);
 
   const handleTextChange = (e) => {
-
-    if(text === '') {
-      setBtnDisabled(true)
-      setMessage(null)
-    } else if (text !=='' && text.trim().length <= 9) {
+    if (text === "") {
       setBtnDisabled(true);
-      setMessage('Text is too short');
-    } else if (text !== "" && text.trim().length >= 100) {
-      setBtnDisabled(true);
-      setMessage("Text is too long");
-    } else {
-      setBtnDisabled(false);
       setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be at least 10 characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
     }
 
-    setText(e.target.value)
-  }
+    setText(e.target.value);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    if(text.trim().length > 9) {
+    e.preventDefault();
+    if (text.trim().length > 10) {
       const newFeedback = {
         text,
-        rating
-      }
+        rating,
+      };
 
-
-      if(feedbackEdit.edit === true) {
-        updateFeedback(feedbackEdit.edit.id, newFeedback)
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback);
       } else {
-          addFeedback(newFeedback);
+        addFeedback(newFeedback);
       }
-      setText('')
+
+      setText("");
     }
-  }
+  };
 
   return (
     <Card>
       <form onSubmit={handleSubmit}>
-        <h2>How would you rate this service?</h2>
+        <h2>How would you rate your service with us?</h2>
         <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
-          <input  onChange={handleTextChange} type="text" placeholder=" Write your review" value={text} />
-          <Button type="submit" isDisabled={btnDisabled}>Submit</Button>
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
 
-        {message && <div className='message'>{message}</div> }
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
-  )
+  );
 }
 
-
-
-
-export default FeedbackForm
+export default FeedbackForm;
